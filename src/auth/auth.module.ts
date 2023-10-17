27547a2ec034829c3978bc903fdc327404bb0b88
sandeepr-19 <sandeep.rqs@gmail.com>
2023-10-17 22:59:57 +0530

    chore(package): remove unused packages

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
     JwtModule.register({
       secret:
         'bWgKGQicQCE2RSPuVnreFSTfFufcsiMEf4jViG-fTuSsezYOVEiRuzpDxXgi7slWY',
